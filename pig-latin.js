c29ecc93830af5fd25baca6fdc5faba3ff710a30
Kanojah <kanoa.hi@gmail.com>
2017-03-21 12:37:48 -1000

    should translate both ways

--- pig-latin.js
+++ pig-latin.js
@@ -6,21 +6,23 @@ function toPigLatin () {
     for (var i = 0; i < word.length; i++) {
       if (word[0]==='a' || word[0]==='e' || word[0]==='i' || word[0]==='o' || word[0]==='u') {
         return true;
+      } else if (word[0]==='y') {
+        return null;
       }
         return false;
-    }
+      }
   }
 
   function toPig (input) {
-      input += '-';
-      while (isVowel(input) === false) {
-        var word = input.split("");
-        var firstSound = word.shift();
-        word.push(firstSound);
-        input = word.join("");
-      }
+    input += '-';
+    while (isVowel(input) === false) {
+      var word = input.split("");
+      var firstSound = word.shift();
+      word.push(firstSound);
+      input = word.join("");
+    }
       input += 'ay';
-      return input;
+        return input;
   }
 
   function translate (input) {
@@ -42,6 +44,32 @@ function toPigLatin () {
 
 function fromPigLatin () {
 
+  function fromPig (input) {
+    var word = input.split("");
+    var i = 0;
+    do {
+      i++;
+    } while (word[i] !== '-');
+    var firstSound = word.splice(i);
+    var firstLetter = firstSound.splice(1, firstSound.length - 3).join("");
+    word.unshift(firstLetter);
+    return word.join("");
+  }
+
+  function translate (input) {
+    var sentence = input.split(" ");
+    var words = [];
+    for (var i = 0; i < sentence.length; i++) {
+      var word = fromPig(sentence[i]);
+      words.push(word);
+    }
+    return words.join(" ");
+  }
+
+  return {
+    fromPig: fromPig,
+    translate: translate
+  };
 }
-var yeah = toPigLatin();
-console.log(yeah.translate('Hey There Uganda'));
+var yeah = fromPigLatin();
+console.log(yeah.translate('ey-hay ou-yay otherfucker-may'));
