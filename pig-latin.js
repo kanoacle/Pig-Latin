ffdc44d5ca231a90338d3f1c612234ddd159260d
Kanojah <kanoa.hi@gmail.com>
2017-03-21 10:57:26 -1000

    just trying to get it to translate back from pig latin

--- pig-latin.js
+++ pig-latin.js
@@ -1,51 +1,47 @@
-function pigLatinTranslator () {
+function toPigLatin () {
 
-  function toPigLatin () {
-
-    function isVowel (input) {
-      var word = input.split("");
-      for (var i = 0; i < word.length; i++) {
-        if (word[0]==='a' || word[0]==='e' || word[0]==='i' || word[0]==='o' || word[0]==='u') {
-          return true;
-        }
-          return false;
+  function isVowel (input) {
+    input = input.toLowerCase();
+    var word = input.split("");
+    for (var i = 0; i < word.length; i++) {
+      if (word[0]==='a' || word[0]==='e' || word[0]==='i' || word[0]==='o' || word[0]==='u') {
+        return true;
       }
+        return false;
     }
+  }
 
-    function toPig (input) {
-      if (isVowel(input) === true) {
-        input += '-ay';
-        return input;
-      }
+  function toPig (input) {
+      input += '-';
+      while (isVowel(input) === false) {
         var word = input.split("");
-        var firstLetter = word.shift();
-        word.push('-' + firstLetter + 'ay');
-        var output = word.join("");
-        return output;
-    }
-
-    function translate (input) {
-      var sentence = input.split(" ");
-      var words = [];
-      for (var i = 0; i < sentence.length; i++) {
-        var word = toPig(sentence[i]);
-        words.push(word);
+        var firstSound = word.shift();
+        word.push(firstSound);
+        input = word.join("");
       }
-      return words.join(" ");
-    }
+      input += 'ay';
+      return input;
+  }
 
-      return {
-        isVowel: isVowel,
-        toPig: toPig,
-        translate: translate
-      };
+  function translate (input) {
+    var sentence = input.split(" ");
+    var words = [];
+    for (var i = 0; i < sentence.length; i++) {
+      var word = toPig(sentence[i]);
+      words.push(word);
+    }
+    return words.join(" ");
   }
 
   return {
-    toPigLatin: toPigLatin()
+    isVowel: isVowel,
+    toPig: toPig,
+    translate: translate
   };
 }
 
+function fromPigLatin () {
 
-var hi = pigLatinTranslator();
-console.log(hi.toPigLatin.translate("hi"));
+}
+var yeah = toPigLatin();
+console.log(yeah.translate('Hey There Uganda'));
